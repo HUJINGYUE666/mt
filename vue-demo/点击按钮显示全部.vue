5c3491907662b5f1cde463c06c277fc2dc4f0513
hujingyue <591711447@qq.com>
2019-09-08 19:27:03 +0800

    test

---
+++ vue-demo/点击按钮显示全部.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div v-for="(item, key) in list" v-show="key<num">{{item}}</div>
+    <span @click="showMore">{{txt}}</span>
+  </div>
+</template>
+<script src="https://unpkg.com/vue"></script>
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      list: ["111", "222", "333", "444", "555", "666"],
+      isShow: true,
+      txt: "显示全部",
+      num: 2
+    };
+  },
+  methods: {
+    showMore() {
+      this.isShow = !this.isShow;
+      this.num = this.isShow ? 2 : this.list.length;
+      this.txt = this.isShow ? "显示全部" : "收起";
+    }
+  }
+};
+</script>
+
+<style  lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ vue-demo/锚点.vue
@@ -0,0 +1,70 @@
+ <template>
+  <div>
+    <li @click="returnCom">快速评论</li>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div id="comment">2222</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    returnCom() {
+      const returnEle = document.querySelector("#comment");
+      if (!!returnEle) {
+        returnEle.scrollIntoView(true);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="">
+</style>
